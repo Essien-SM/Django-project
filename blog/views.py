1f4af0b1aefbeafbc662b30599aae9a502c5567f
Essien-SM <stephenyessien@gmail.com>
2022-04-09 19:47:55 -0700

    template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+    'author': 'Essien',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'April 24, 2022'
+    },
+    {
+    'author': 'Stephen',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 25, 2022'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About </h1>')
+    return render(request,'blog/about.html', {'title': 'About'})
+    
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\AZIZ\Documents\my_django_app\django_project\blog\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
